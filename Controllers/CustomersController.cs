39a4845a55e1fa6f1c11c496c0ce1008ffd06dd2
javtheghost <callofgamer34@gmail.com>
2024-04-14 23:15:32 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using PRACTICA__.Data;
+using PRACTICA__.Models;
+
+namespace PRACTICA__.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: CustomerController
+
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Index()
+        {
+            IEnumerable<Customers> listaCustomers = _context.customers;
+            return View(listaCustomers);
+        }
+
+        // GET: CustomerController/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = _context.customers.FirstOrDefault(c => c.CUSTOMER_ID == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // GET: CustomerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Customers customers)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.customers.Add(customers);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customers);
+        }
+
+        // GET: CustomerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var cust = _context.customers.Find(id);
+            return View(cust);
+        }
+
+        // POST: CustomerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Customers customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.customers.Update(customer);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // GET: CustomerController/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = _context.customers.FirstOrDefault(c => c.CUSTOMER_ID == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // POST: CustomerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            var customer = _context.customers.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            _context.customers.Remove(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
